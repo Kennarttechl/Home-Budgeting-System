599663337298a61b3325aaeafa3a1c2cad6cb04e
Kennarttechl <kennarttechnologies@gmail.com>
2024-01-29 20:55:35 -0800

    Refactor all

--- home_buget/auth/form.py
+++ home_buget/auth/form.py
@@ -5,10 +5,14 @@ from wtforms.validators import DataRequired, ValidationError, Length, EqualTo
 
 class RegistterForm(FlaskForm):
     username = StringField(
-        validators=[DataRequired(), Length(15)],
+        validators=[DataRequired(), Length(min=1, max=15)],
         render_kw={"placeholder": "Enter username"},
     )
     password = PasswordField(
-        validators=[DataRequired()], render_kw={"placeholder": "Enter your password"}
+        validators=[DataRequired(), Length(min=1, max=15)],
+        render_kw={"placeholder": "Enter your password"},
     )
-    confirm_password = PasswordField(validators=[DataRequired(), EqualTo("password")])
+    confirm_password = PasswordField(
+        validators=[DataRequired(), EqualTo("password")],
+        render_kw={"placeholder": "Confirm_Password"},
+    )
